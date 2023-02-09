25e2a718d3847ca6042e77f44d85e71a4d3ea714
Jack PC <jwooler1@gmail.com>
2023-02-09 14:40:11 +0000

    first commit

---
+++ libs/script.js
@@ -0,0 +1,483 @@
+//panzoom/----------------------------------------------------------------------------------------------------------------------------------------/
+$( document ).ready(function() {
+    const mapElement = $(".map .svg")[0];
+    const panzoom = Panzoom(mapElement, { contain: 'outside', startScale: 1.5 });
+  
+    mapElement.parentElement.addEventListener('wheel', panzoom.zoomWithWheel)
+    mapElement.parentElement.addEventListener('wheel', function (event) {
+    if (!event.shiftKey) return
+    panzoom.zoomWithWheel(event)
+    
+  })
+});
+
+
+
+//global variables/----------------------------------------------------------------------------------------------------------------------------------------/
+var civilisationArray = [];
+
+
+//-------------------------------------------------------------
+//troop types (json statsheets)
+let knight = {attacks:3, strength:5, save:5, cost:5}; //like 40k, using math.random as dice, 3+ saves etc.
+let archer = {attacks:6, strength:5, save:6, cost:10};
+let horseman = {attacks:1, strength:3, save:3, cost:15};
+
+
+
+//Auto increaes/----------------------------------------------------------------------------------------------------------------------------------------/
+
+//-------------------------------------------------------------
+//starting money
+let money = 500;
+$("#eboMoney").text(money);
+
+//-------------------------------------------------------------
+// Add money ever x seconds 
+window.setInterval(function () {
+    var money = parseInt($('#eboMoney').text());
+    $('#eboMoney').text(money+1);
+}, 5000);
+
+
+
+//main stats JSONS/----------------------------------------------------------------------------------------------------------------------------------------/
+
+
+//-------------------------------------------------------------
+//county statistics (json statsheets)
+
+function createCivilisations() {
+
+    var paths = document.getElementsByTagName("path");
+    var pathsArray = []
+    
+    for (let i=0; i < paths.length; i++){
+
+            pathsArray.push($(paths[i]).attr('name'))
+            
+            civilisationArray.push({
+                id: i,
+                name: pathsArray[i],
+                economy: 1,
+                buildings: {
+                    barracks: 0,
+                    market: 2,
+                    castle: 1
+                    },
+                military: {
+                    knights: 1,
+                    archers: 5,
+                    horsemen: 1
+                    },
+                population: 100,
+                diplomacy: {
+                    allies: ['Rome', ' Greece'],
+                    enemies: ['Parthians'],
+                    neutral: ['Gaul']
+                }
+            })
+        
+            $(paths[i]).attr('id', i)
+
+        }
+}
+
+
+
+//fight functions/----------------------------------------------------------------------------------------------------------------------------------------/
+
+
+//-------------------------------------------------------------
+//atacking function
+function attacking(aggressor, defenderNumber){
+    let diceRoll = Math.floor(Math.random()* 6) +1;
+    if (diceRoll > aggressor.strength) {
+        let eboKnights = parseInt($(defenderNumber).text());
+        $(defenderNumber).text(eboKnights-1);
+    } else {
+        alert("you missed")
+    }
+}
+
+
+$("#fightDover").on("click", function() {
+    attacking(knight, '#eboKnights')
+})
+
+
+// this would be a nice way of tidying the code up
+function fighting(
+                knightsNumberA, 
+                archersNumberA, 
+                horseNumberA, 
+                knightsNumberB, 
+                archersNumberB, 
+                horsenumberB 
+) 
+            {
+
+
+}
+
+$("#fight").on("click", function() {
+
+    let knightsA = parseInt($('#knightsA').text()); 
+    let archersA = parseInt($('#archersA').text()); 
+    let horseA = parseInt($('#horseA').text());
+
+    let knightsB = parseInt($('#knightsB').text()); 
+    let archersB = parseInt($('#archersB').text()); 
+    let horseB = parseInt($('#horseB').text());
+    
+    let knightsAttacksA = parseInt(knightsA * knight.attacks);
+    let archersAttacksA = parseInt(archersA * archer.attacks);
+    let horseAttacksA = parseInt(horseA * horseman.attacks);
+
+    let knightsAttacksB = parseInt(knightsB * knight.attacks);
+    let archersAttacksB = parseInt(archersB * archer.attacks);
+    let horseAttacksB = parseInt(horseB * horseman.attacks);
+
+    //a
+
+    for (let i = 0; i < archersAttacksA; i++) {
+        
+        let diceRoll = Math.floor(Math.random()* 6) +1;
+        let saveRoll = Math.floor(Math.random()* 6) +1;
+        
+        if (diceRoll > archer.strength && saveRoll > archer.save) {
+
+            if (knightsB > 0) {
+                $('#knightsB').text(knightsB-1); 
+                console.log("You win!")  
+            } else if (archersB > 0) {
+                $('#archersB').text(archersB-1);
+            } else if (horseB > 0) {
+                $('#horseB').text(horseB-1);
+            } else {
+                console.log("You win!")
+            }
+
+        } else {
+            console.log("archerA miss")
+        }
+    }
+
+    for (let i = 0; i < horseAttacksA; i++) {
+        
+        let diceRoll = Math.floor(Math.random()* 6) +1;
+        let saveRoll = Math.floor(Math.random()* 6) +1;
+        
+        if (diceRoll > horseman.strength && saveRoll > horseman.save) {
+
+            if (knightsB > 0) {
+                $('#knightsB').text(knightsB-1);
+            } else if (archersB > 0) {
+                $('#archersB').text(archersB-1);
+            } else if (horseB > 0) {
+                $('#horseB').text(horseB-1);
+            } else {
+                console.log("You win!")
+            }
+
+        } else {
+            console.log("horseA miss")
+        }
+    }
+
+    for (let i = 0; i < knightsAttacksA; i++) {
+        
+        let diceRoll = Math.floor(Math.random()* 6) +1;
+        let saveRoll = Math.floor(Math.random()* 6) +1;
+        
+        if (diceRoll > knight.strength && saveRoll > knight.save) {
+
+            if (knightsB != -1) {
+                $('#knightsB').text(knightsB--);
+                console.log("knight killed knight")   
+            } else if (archersB != -1) {
+                $('#archersB').text(archersB--);
+                console.log("knight killed archer")
+            } else if (horseB != -1) {
+                $('#horseB').text(horseB--);
+                console.log("knight killed horse")
+            } else {
+                console.log("You win!")
+            }
+
+        } else {
+            console.log("knightA miss")
+        }
+    }
+
+    //b
+    
+    for (let i = 0; i < archersAttacksB; i++) {
+        
+        let diceRoll = Math.floor(Math.random()* 6) +1;
+        let saveRoll = Math.floor(Math.random()* 6) +1;
+        
+        if (diceRoll > archer.strength && saveRoll > archer.save) {
+
+            if (knightsA > 0) {
+                $('#knightsA').text(knightsA-1); 
+                console.log("You win!")  
+            } else if (archersA > 0) {
+                $('#archersA').text(archersA-1);
+            } else if (horseA > 0) {
+                $('#horseA').text(horseA-1);
+            } else {
+                console.log("You win!")
+            }
+
+        } else {
+            console.log("archerA miss")
+        }
+    }
+
+    for (let i = 0; i < horseAttacksB; i++) {
+        
+        let diceRoll = Math.floor(Math.random()* 6) +1;
+        let saveRoll = Math.floor(Math.random()* 6) +1;
+        
+        if (diceRoll > horseman.strength && saveRoll > horseman.save) {
+
+            if (knightsA > 0) {
+                $('#knightsA').text(knightsA-1);
+            } else if (archersA > 0) {
+                $('#archersA').text(archersA-1);
+            } else if (horseA > 0) {
+                $('#horseA').text(horseA-1);
+            } else {
+                console.log("You win!")
+            }
+
+        } else {
+            console.log("horseA miss")
+        }
+    }
+
+    for (let i = 0; i < knightsAttacksB; i++) {
+        
+        let diceRoll = Math.floor(Math.random()* 6) +1;
+        let saveRoll = Math.floor(Math.random()* 6) +1;
+        
+        if (diceRoll > knight.strength && saveRoll > knight.save) {
+
+            if (knightsA != -1) {
+                $('#knightsA').text(knightsA--);
+                console.log("knight killed knight")   
+            } else if (archersA != -1) {
+                $('#archersA').text(archersA--);
+                console.log("knight killed archer")
+            } else if (horseA != -1) {
+                $('#horseA').text(horseA--);
+                console.log("knight killed horse")
+            } else {
+                console.log("You win!")
+            }
+
+        } else {
+            console.log("knightA miss")
+        }
+    }
+
+})
+
+
+
+//move units/----------------------------------------------------------------------------------------------------------------------------------------/
+
+
+//-------------------------------------------------------------
+//move units function
+function moveUnit(oldLocation, newLocation) {
+    let unitAmount = parseInt($(oldLocation).text());
+    let newAmount = parseInt($(newLocation).text());
+    $(newLocation).text(newAmount+unitAmount);
+    $(oldLocation).text(unitAmount-unitAmount);
+}
+
+
+$("#knightFromEboToBrighton").click(function() {
+    moveUnit("#eboKnights", "#btownKnights" )
+})
+
+$("#knightFromBrightonToHastings").click(function() {
+    moveUnit("#btownKnights", "#hastingsKnights" )
+})
+
+
+
+$("#moveAllToFightA").click(function() {
+    moveUnit("#eboKnights", "#knightsA" )
+})
+
+$("#moveAllToFightB").click(function() {
+    moveUnit("#btownKnights", "#knightsB" )
+})
+
+
+
+//buy units/----------------------------------------------------------------------------------------------------------------------------------------/
+
+
+//-------------------------------------------------------------
+//buy function
+function buy(unitCost, unitLocation) {
+    
+    let location = parseInt($(unitLocation).text());
+    $(unitLocation).text(location+1); 
+
+    let money = parseInt($('#eboMoney').text());
+    $('#eboMoney').text(money - unitCost); 
+
+}
+
+$("#buyKnight").click(function() {
+    buy(knight.cost, "#eboKnights")
+})
+
+$("#buyArcher").click(function() {
+    buy(archer.cost, "#eboArchers")
+})
+
+$("#buyHorse").click(function() {
+    buy(horse.cost, "#eboHorse")
+})
+
+
+
+//on clicks/----------------------------------------------------------------------------------------------------------------------------------------/
+
+
+//-------------------------------------------------------------
+// Click on country borders
+$("#GBR2677").on("click", function() {
+    $("#GBR2677").attr('fill', 'blue');
+})
+
+
+//Find nearest ID and change colour
+$( "#svg" ).on("click", function( event ) {
+
+    
+    //---------------------------------------------------
+    //County sidepanel text
+
+
+
+        //nearest name / colour
+        //------------------
+        
+        //variables
+        var nearestName = $(event.target).attr('name');
+        let nearestID = (event.target.id);
+        let nearest = "#" + nearestID;
+        let nearestInt = parseInt(nearestID);
+     
+        
+
+        //nearest colour
+        const paths = document.getElementsByTagName("path");
+        
+        for (let i=0; i < paths.length; i++){
+                $(paths[i]).attr('fill', '#6E8898')
+            }
+
+        $('#exampleModal').modal('toggle');
+        $(nearest).attr('fill', '#2E5266')
+
+
+        //resets
+        //--------------------
+
+        //top right
+        $("#location").text('')
+
+        
+        //military sidebar reset
+        $('#countyHeader').text('Choose a county')
+        
+        $('#modalTitle').text(nearestName)
+        $('#countyHeader').text(nearestName)
+        
+        $('#countyBody').empty();
+        $('#countyBody').append(
+            '<h1>Military</h1>',
+            '<p>Knights: <span id="knights"></span></p>',
+            '<p>Archers: <span id="archers"></span></p>',
+            '<p>Horsemen: <span id="horsemen"></span></p>',
+            '<button id="buyKnightCounty">Buy Knight</button>'
+        )
+        $("#buyKnightCounty").on("click", function() {
+            let oldKnights = civilisationArray[nearestInt].military.knights;
+            civilisationArray[nearestInt].military.knights = oldKnights + 1;
+            $("#knights").text(civilisationArray[nearestInt].military.knights)
+            //needs to cost too (rebuild buy function)
+        })
+
+        
+        //buildings sidebar reset
+        $('#countyBuildingsHeader').text('Choose a county')
+        $('#countyBuildingsBody').empty();
+        $('#countyBuildingsBody').append(
+            '<h1>Buildings</h1>',
+            '<p>Barracks: <span id="barracks"></span></p>',
+            '<p>Market: <span id="market"></span></p>',
+            '<p>Castles: <span id="castle"></span></p>'
+        )
+        
+        //diplomacy sidebar reset
+        $('#countyDiplomacyHeader').text('Choose a county')
+        $('#countyDiplomacyBody').empty();
+        $('#countyDiplomacyBody').append(
+            '<h1>Diplomacy</h1>',
+            '<p>Allies: <span id="allies"></span></p>',
+            '<p>Enemies: <span id="enemies"></span></p>',
+            '<p>Neutrual: <span id="neutral"></span></p>'
+        )
+
+
+        //sidebar content filling
+        //--------------------------------
+        
+        //military body text
+        $("#location").text(nearestName);
+        $("#pathsName").text(civilisationArray[nearestInt].name)
+        $("#knights").text(civilisationArray[nearestInt].military.knights)
+        $("#archers").text(civilisationArray[nearestInt].military.archers)
+        $("#horsemen").text(civilisationArray[nearestInt].military.horsemen)
+
+        //county buildings body text
+        $('#countyBuildingsHeader').text(civilisationArray[nearestInt].name)
+        $("#barracks").text(civilisationArray[nearestInt].buildings.barracks)
+        $("#market").text(civilisationArray[nearestInt].buildings.market)
+        $("#castle").text(civilisationArray[nearestInt].buildings.castle)
+
+        //county diplomacy body text
+        $('#countyDiplomacyHeader').text(civilisationArray[nearestInt].name)
+        $("#allies").text(civilisationArray[nearestInt].diplomacy.allies)
+        $("#enemies").text(civilisationArray[nearestInt].diplomacy.enemies)
+        $("#neutral").text(civilisationArray[nearestInt].diplomacy.neutral)
+
+})
+
+
+
+//modals/----------------------------------------------------------------------------------------------------------------------------------------/
+
+//Find nearest ID and create a modal with its info on
+function buildModal() {
+    
+}
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
+
+
+
+//starting functions/----------------------------------------------------------------------------------------------------------------------------------------/
+
+createCivilisations()
---
+++ notes/notes.md
@@ -0,0 +1,226 @@
+First jobs
+----------
+
+- Change the buy unit functions to use the money from that IDs JSON (money: $10 etc.)
+- This adds to the unit amount in that IDs JSON ([i].military.archers+1 etc.)
+- Then it runs the function that updates the side county panel
+
+- Do the same for a few test buildings
+- Their functions can be added in later (i.e roll modifiers)
+
+- add all the other bits for the side panel (unit types, amount of buildnigs etc.)
+- have these on different pages / side bars (e.g a buildings one, a military one, a diplomacy one)
+
+- The next jobs after that will be to update the fight functions 
+- They'll need to use the JSON data instead of the html
+- Maybe give this pop up / modal / animation if it works well
+- Says if you win/lose and by how much
+
+
+
+Notes 
+----------
+
+map generator 
+
+- could use the below. It's quite clean
+- https://github.com/stravid/mapgenerator
+- it also sorts the adjacent tiles stuff
+
+
+time
+
+- Have a time / date counter
+- Every xxx days, roll a dice for each county. If that roll > xxx, increase their population etc.
+- Have roll modifiers for certain building types / cultures 
+
+buildings
+
+- As part of the JSON, have buildings
+- I.e. markets: 1 / barracks: 2 / castle: 1
+- market would give you a modifier for how much a county earns 
+- have tech level that unlocks certain buildings (also depends if you have the version before (e.g. barracks level 1/2/3)
+
+AI wars
+
+- Eventually have low chance dice rolls that they'll auto fight any neighbours they aren't allied with
+- This means the JSON needs 'allied', 'neutral' & 'enemy' arrarys with relevant IDs in each
+
+culture / player
+
+- have a culture stat (e.g. roman / greek) that gives you certain bonuses / colours
+- have ownedBy stat which gives the path fill of your faction
+- later these will control who you have control of and who you can fight)
+
+buy/fight buttons
+
+- create buttons that buy units for that county
+- button to fight (with dynamically created list that dictates who you can fight)
+- buttons to buy / upgrade buildings
+
+
+
+
+older ideas 
+-----------
+
+- rts with 40k style dice roles
+- potentially as rpg, as if commander on battfield with same text style as Darlene?
+- if not, go full div style grid but with 40k style logic
+- still have the issue of AI
+
+- one way you could get rid of AI issues is having it be more survival
+- like Banished, you just need to have enough food etc.
+- for the fights, you could just have orc raids?
+- make it more of a very miniturised dwarf fortress clone instead
+- more horror instead of fantasy though, weird aliens after you crash land?
+- would be a cool way of introducing narrative to RTS, giving you more story each season you survive
+- the full map would be easier graphics though
+- could go more for ants?
+- make different kinds, attack, strength ants etc. and have wars
+- AI would just get allocated a random rate of expansion
+
+
+- ever increasing money
+- same for AI enemies
+- financial policies etc. (taxation?)
+
+- map
+- towns/cities
+
+- attack settlement
+- move from one to another
+- take a certain amount of time to do so, depending on how far 
+- make enemies do this automatically every x time goes by
+ 
+- build units
+- build buildings
+- take over settlment 
+
+- weather / seasons
+- i.e. no movement in winter without serious detriment
+
+- maybe have food as well, which increases depending on buildings, grows less in winter
+- electricity if scifi, which you need power buildings for etc.
+- or replace this with religion, so you need churches (piety)
+- clothing / drink / tools / pets / fresh water / sugar / tobacco / slaves / workers
+
+- you could have people die in old age, and if you don't have places to bury them people are sad and can do less
+
+- I think having it be more like dwarf fortress or Crusader Kings, where the world and its inhabitants would move as if you're not there anyway.
+- It's just a giant simulation that you can take a part in, and you'll usually die but that's fun too.
+- A cool theme could be landing on Navan 1,000 years ago with the first Order ships.
+- You get a load of good stuff, but you won't get any support for a full rote. 
+- There are inhabitants there, some savages perhaps, some monstrous creatures. 
+- Or it could be an old colony that the order had lost touch with, assumed it had just failed and lost contact.
+- Depending on how much piety / prestige you get in that year depends what the Order will send you next year. 
+
+- On completing the game, there are a ton of if/else statements
+- if < x of your population was enslaved, you get a nice ending
+- if > x you get a nasty ending 
+- if > x survived from the original party you get certain text
+- And so on
+
+
+
+
+
+//-------------------------------------------------------------
+// old commented code
+
+
+// $("#svg").on("hover", function( event ) {
+
+//     let nearestID = (event.target.id);
+//     let nearest = "#" + nearestID;
+//     console.log(nearest)
+
+//     console.log(event.target)
+    
+
+//     $(nearest).attr('fill', 'black')
+    
+// });
+
+
+// $("#buyKnight").click(function() {
+//     let eboMoney = $("#eboMoney").html();
+    
+//     if (eboMoney > 4) {
+//         let eboKnights = parseInt($('#eboKnights').text());
+//         $("#eboKnights").text(eboKnights+1);
+
+//         let beforeMoney = $("#eboMoney").html(); 
+//         let afterMoney = beforeMoney - 5;
+//         $("#eboMoney").html(afterMoney);
+
+//     } else {
+//         alert("You can't afford that.")
+//     }
+// })
+
+// //buy archer
+// $("#buyArcher").click(function() {
+//     let eboMoney = $("#eboMoney").html();
+    
+//     if (eboMoney > 4) {
+//         let eboArchers = parseInt($('#eboArchers').text());
+//         $("#eboArchers").text(eboArchers+1);
+
+//         let beforeMoney = $("#eboMoney").html(); 
+//         let afterMoney = beforeMoney - 10;
+//         $("#eboMoney").html(afterMoney);
+
+//     } else {
+//         alert("You can't afford that.")
+//     }
+// })
+
+ // //roll dice
+    // for (i = 0; i < 20; i++) {
+    //     let diceRoll = Math.floor(Math.random()* 6) +1;
+    //     console.log(diceRoll);
+    // }
+    
+    
+
+    // let eboArchers = parseInt($('#eboArchers').text());
+
+    // //if ()
+
+    // let attacker = 1;
+
+    // let defender = 2;
+
+    // function fight(attacker, defender) {
+    //     if (attacker > defender) {
+    //         alert("attacker one")
+    //     } else {
+    //         alert("attacker lost")
+    //     }
+    // }
+
+
+
+    //in testing/----------------------------------------------------------------------------------------------------------------------------------------/
+
+
+// //-------------------------------------------------------------
+// // create county variables for each path
+
+var countyData = []
+
+function createCountyVariables(){
+    
+        var paths = document.getElementsByTagName("path");
+
+        for (let i=0; i < paths.length; i++) {
+            //countyData.push()
+             var paths = document.getElementsByTagName("path");
+             countyData.push($(paths).attr('name'));
+            
+         }     
+}
+
+//county variables function (in testing - not that necessary now?)
+    createCountyVariables()
